af7cbbc019e22a36cb6bae8f773236b495ed5ace
Joshua Yang <joshuayang@mac.com>
2022-08-11 14:36:20 +0800

    Initial Commit

---
+++ Pathfinding Visualizer/Pathfinding_VisualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pathfinding_VisualizerApp.swift
+//  Pathfinding Visualizer
+//
+//  Created by Joshua Yang on 2022/8/11.
+//
+
+import SwiftUI
+
+@main
+struct Pathfinding_VisualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
