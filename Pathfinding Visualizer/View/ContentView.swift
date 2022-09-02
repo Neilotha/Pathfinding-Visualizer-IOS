658b2887fed8351c10cc938248e6446f788a5422
Joshua Yang <joshuayang@mac.com>
2022-09-02 11:54:22 +0800

    Fixed orientation detection

--- Pathfinding Visualizer/View/ContentView.swift
+++ Pathfinding Visualizer/View/ContentView.swift
@@ -7,6 +7,26 @@
 
 import SwiftUI
 
+enum Orientation {
+    case portrait
+    case landscape
+    
+    
+    mutating func newOrientation(_ orientation: UIDeviceOrientation) -> Bool {
+        var orientationChanged = false
+        if orientation.isPortrait && self != .portrait {
+            orientationChanged = true
+            self = .portrait
+        }
+        else if orientation.isLandscape && self != .landscape {
+            orientationChanged = true
+            self = .landscape
+        }
+        
+        return orientationChanged
+    }
+}
+
 struct DeviceRotationViewModifier: ViewModifier {
     let action: (UIDeviceOrientation) -> Void
 
@@ -27,6 +47,7 @@ extension View {
 
 struct ContentView: View {
     @StateObject var model = GridModel()
+    @State var orientation: Orientation = Orientation.portrait
     
     var body: some View {
         VStack {
@@ -35,9 +56,14 @@ struct ContentView: View {
         
             GeometryReader { geo in
                 GridView(model: self.model, height: Int(geo.size.height), width: Int(geo.size.width))
-                    .onRotate { _ in
-                        model.updateModel(height: Int(geo.size.height), width: Int(geo.size.width))
-                            }
+                    .onRotate { newOrientation in
+                        if self.orientation.newOrientation(newOrientation) {
+                            print(orientation)
+                            model.updateModel(
+                                height: Int(geo.size.height),
+                                width: Int(geo.size.width))
+                        }
+                    }
             }
         }
     }
